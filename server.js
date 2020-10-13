dcff57a1fdf1f1b0eb0190ea773ac5c9ff2c69e1
Yonatan Palagashvili <yonimotek@gmail.com>
2020-10-13 13:30:41 -0700

    Morgan helped me with deployment

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const superagent = require('superagent');
 const app = express();
 const PORT = process.env.PORT;
 app.use(cors());
@@ -16,34 +17,55 @@ app.get('/unauthorized', (request, response) => {
   throw new Error('not authorized to access this route');
 });
 
-
-app.get('/test/route', (request, response) => {
-  response.json({ location: 'seattle', temp: '58 deg' });
-});
-
-
 app.get('/location', handleLocation);
 
 function Location(city, coordinates) {
-  this.search_query = city;
-  this.formatted_query = coordinates[0].display_name;
-  this.latitude = coordinates[0].lat;
-  this.longitude = coordinates[0].lon;
+    this.search_query = city;
+    this.formatted_query = coordinates[0].display_name;
+    this.latitude = coordinates[0].lat;
+    this.longitude = coordinates[0].lon;
 }
 
 function handleLocation(request, response) {
+    try {
+        const coordinates = require('./data/location.json');
+        const city = request.query.city; 
+        const url = `https://us1.locationiq.come/v1/search.php?key=${Location_API_KEY}&q=${city}&format=json&limt=1`
+        superagent.get(url)
+            .then(data => {
+                const coordinates = data.body[0];
+                const locationData = new Location(city,coordinates);
+                locationData(url) = 
+                response.json(locationData)
+            })
+        
+    } catch {
+        response.status(500).send('sorry, something broke.');
+    }
+}
+
+app.get('/weather', handleWeather);
+
+function Weather(day, weatherReport) {
+  this.search_query = day;
+  this.formatted_query = weatherReport[0].display_name;
+  this.latitude = weatherReport[0].lat;
+  
+}
+
+function handleWeather(request, response) {
   try {
     // try to "resolve" the following (no errors)
-    const coordinates = require('./data/location.json');
-    const city = request.query.city; // "seattle" -> localhost:3000/location?city=seattle
-    const locationData = new Location(city, coordinates);
-    response.json(locationData);
+    const weatherReport = require('./data/location.json');
+    const day = request.query.weatherReport; 
+    const weatherData = new Weather(day, weatherReport);
+    response.json(weatherData);
   } catch {
     // otherwise, if an error is handed off, handle it here
     response.status(500).send('sorry, something broke.');
   }
 }
-
+//404 catch all route
 app.get('*', (request, response) => {
   response.status(404).send('not found');
 });
