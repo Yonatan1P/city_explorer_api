84dbf1dd253d548a9332ce98b6f4f0aa9df2662f
Yonatan Palagashvili <yonimotek@gmail.com>
2020-10-13 22:03:26 -0700

    working on lab07 so this is the wrong branch, but I forgot to change it before hand

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # city_explorer_api
 Code Fellows 301d67 Lab06
-Following the trello board: https://trello.com/b/0boJ848b/lab06
+Following the trello board: https://trello.com/b/HNjajHX7/full-city-explorer-board
 
 
 ## feature 1: setting up  the repository
@@ -12,3 +12,7 @@ Start time: __3pm___
 Finish time: __4pm___
 
 Actual time needed to complete: __1hr___
+
+
+# Lab07
+
--- server.js
+++ server.js
@@ -1,15 +1,21 @@
 'use strict';
 
 require('dotenv').config();
+
 const express = require('express');
 const cors = require('cors');
+
 const superagent = require('superagent');
+
 const app = express();
+
 const PORT = process.env.PORT;
+
 app.use(cors());
 
 //homepage
 app.get('/', (request, response) => {
+    console.log('homepage works');
   response.send('my homepage');
 });
 //unauthorized error
@@ -18,58 +24,99 @@ app.get('/unauthorized', (request, response) => {
 });
 
 app.get('/location', handleLocation);
+app.get('/weather', handleWeather);
+app.get('/trails', handleTrails);
+app.get('*', notFoundHandler);
+
+
+function handleLocation(request, response) {
+    const city = request.query.city;
+    const key = process.env.GEOCODE_API_KEY;
+    const url = `https://us1.locationiq.com/v1/search.php?key=${key}&q=${city}&format=json`
+    
+        superagent.get(url)
+        .then(data => {
+            const currentCoordinates = data.body[0];
+            const locationObj = new Location(city, currentCoordinates);
+            response.send(locationObj);
+        })
+        .catch(error => {
+            response.status(500).send('sorry, something went wrong.');
+        });
+      
+}
 
 function Location(city, coordinates) {
     this.search_query = city;
-    this.formatted_query = coordinates[0].display_name;
-    this.latitude = coordinates[0].lat;
-    this.longitude = coordinates[0].lon;
+    this.formatted_query = coordinates.display_name;
+    this.latitude = coordinates.lat;
+    this.longitude = coordinates.lon;
 }
 
-function handleLocation(request, response) {
-    try {
-        const coordinates = require('./data/location.json');
-        const city = request.query.city; 
-        const url = `https://us1.locationiq.come/v1/search.php?key=${Location_API_KEY}&q=${city}&format=json&limt=1`
-        superagent.get(url)
-            .then(data => {
-                const coordinates = data.body[0];
-                const locationData = new Location(city,coordinates);
-                locationData(url) = 
-                response.json(locationData)
-            })
-        
-    } catch {
-        response.status(500).send('sorry, something broke.');
-    }
-}
-
-app.get('/weather', handleWeather);
-
-function Weather(day, weatherReport) {
-  this.search_query = day;
-  this.formatted_query = weatherReport[0].display_name;
-  this.latitude = weatherReport[0].lat;
-  
-}
 
 function handleWeather(request, response) {
-  try {
-    // try to "resolve" the following (no errors)
-    const weatherReport = require('./data/location.json');
-    const day = request.query.weatherReport; 
-    const weatherData = new Weather(day, weatherReport);
-    response.json(weatherData);
-  } catch {
+        const getWeather = request.query.search_query;
+        const weatherKey = WEATHER_API_KEY; 
+        console.log(weatherKey);
+        const url = `https://api.weatherbit.io/v2.0/forecast/daily?key=${weatherKey}&city=${getWeather}`
+       
+        superagent.get(url)
+        .then(results => {
+            const getWeatherArr = [];
+            const weatherReport = results.body.data.forcast[0];
+            const weatherObj = new Weather (day, weatherReport);
+            locations[url] = weatherObj;
+            response.json(weatherObj);
+        })
+        .catch(error => {
+            response.status(500).send('sorry, something went wrong.');
+        });
     // otherwise, if an error is handed off, handle it here
     response.status(500).send('sorry, something broke.');
-  }
-}
-//404 catch all route
-app.get('*', (request, response) => {
-  response.status(404).send('not found');
-});
 
+}
+
+function Weather(data) {
+  this.forcast = data.weather.description;
+  this.time = data.datetime;
+}
+
+function handleTrails(request, response) {
+    const city = request.query.city;
+    const key = process.env.TRAIL_API_KEY;
+    const url = `https://us1.locationiq.com/v1/search.php?key=${key}&q=${city}&format=json`
+
+        superagent.get(url)
+        .then(data => {
+            const currentCoordinates = data.body[0];
+            const trailObj = new Trail(city, currentCoordinates);
+           
+            response.send(trailObj);
+        })
+        .catch(error => {
+            response.status(500).send('sorry, something went wrong.');
+        });  
+}
+
+function Trail(data) {
+    this.name = data.name;
+    this.type = data.type;
+    this.summary = data.summary;
+    this.location = data.location;
+    this.length = data.length;
+    this.stars = data.star;
+    this.star_votes = data.star_votes;
+    this.trail_url = data.url;
+    this.conditions = data.conditionDetails;
+    this.conditionDate = data.conditionDate;
+    this.conditionTime = data.conditionTime
+}
+
+//404 catch all route
+function notFoundHandler (request, response) {
+    response.status(404).send('not found');
+};
+//let us know the computer is listening to the port
 app.listen(PORT, () => {
   console.log(`server up: ${PORT}`);
 });
